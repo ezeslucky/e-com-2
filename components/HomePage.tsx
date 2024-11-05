9fc51ce3b931da346c7ec87d320e67edc1e3ecfa
Lucky <lucky123@gmail.com>
2024-11-05 06:10:35 +0530

    e-com

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import Image from "next/image";
 import { useSupabase } from '@/lib/supabase/hooks/useSupabase';
 import CategoryWiseProduct from './shared/CategoryWiseProduct';
-import Link from "next/link";
+
 
 const HomePage = () => {
   const { mensProduct,
