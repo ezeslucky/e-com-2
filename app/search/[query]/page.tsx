2fd2385b3d1d5c411dd989c5e75e86a49afd676e
Lucky <lucky123@gmail.com>
2024-11-05 05:36:28 +0530

    e-com

---
+++ app/search/[query]/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+import SearchResult from '@/components/SearchResult';
+import { useSupabase } from '@/lib/supabase/hooks/useSupabase';
+import { useParams } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+const SearchPage = () => {
+    const { query } = useParams();
+    const {filterData,getFilteredData} = useSupabase();
+
+    useEffect(()=>{
+        //@ts-ignore
+        getFilteredData(query.toString());
+    },[query,getFilteredData]);
+   
+ 
+    
+    return (
+        <div>
+            <SearchResult filterData = {filterData}/>
+        </div>
+    )
+}
+
+export default SearchPage
